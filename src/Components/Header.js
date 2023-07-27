a3d9db046f23b99ab5e7fe9ef0a621259c9c26fe
Di M <meerkdir@gmail.com>
2023-07-27 14:17:53 +0200

    Menu fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -36,21 +36,14 @@ function Header() {
                     <Offcanvas.Body>
                         <Nav className="justify-content-end flex-grow-1">
                             <Nav.Link href="https://www.africanobservatory.ai/about">About</Nav.Link>
-                            <Nav.Link href="https://www.africanobservatory.ai/resources">Resources</Nav.Link>
+                            <Nav.Link href="https://research.africanobservatory.ai/">Research Directory</Nav.Link>
+                            <Nav.Link href="https://policy.africanobservatory.ai/">Policy Map</Nav.Link>
                             <Nav.Link href="https://www.africanobservatory.ai/projects">Projects</Nav.Link>
                             <Nav.Link href="https://www.africanobservatory.ai/social">Social</Nav.Link>
                             <Nav.Link href="https://www.africanobservatory.ai/women-in-focus">Women in focus</Nav.Link>
                         </Nav>
                     </Offcanvas.Body>
                 </Navbar.Offcanvas>
-                
-                {/* <Nav className="me-auto">
-                    <Nav.Link href="https://www.africanobservatory.ai/about">About</Nav.Link>
-                    <Nav.Link href="https://www.africanobservatory.ai/resources">Resources</Nav.Link>
-                    <Nav.Link href="https://www.africanobservatory.ai/projects">Projects</Nav.Link>
-                    <Nav.Link href="https://www.africanobservatory.ai/social">Social</Nav.Link>
-                    <Nav.Link href="https://www.africanobservatory.ai/women-in-focus">Women in focus</Nav.Link>
-                </Nav> */}
             </Container>
         </Navbar>
     )
